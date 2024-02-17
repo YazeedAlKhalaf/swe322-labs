2ce9117e5b71cfea1e6457b409c6be1534e8af7d
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-02-17 12:31:34 +0300

    fix the things

---
+++ lab1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lab 1 Stuff</title>
+  </head>
+  <body>
+    <ol>
+      <li><a href="first/index.html">First</a></li>
+      <li><a href="second/index.html">Second</a></li>
+      <li><a href="third/index.html">Third</a></li>
+    </ol>
+  </body>
+</html>
