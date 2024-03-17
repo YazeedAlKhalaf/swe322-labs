461a36aeb1fcd59683f6be558ff05971d88f6464
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-03-17 22:18:06 +0300

    fix some stuff

--- lab4/index.php
+++ lab4/index.php
@@ -4,18 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab 3</title>
+    <title>Lab 4</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="min-h-screen max-w-[48rem] m-auto flex flex-col">
-    <h1 class="text-white bg-blue-500 p-4 font-bold text-3xl">Lab 3</h1>
+    <h1 class="text-white bg-blue-500 p-4 font-bold text-3xl">Lab4</h1>
     <main class="p-4 flex-1">
         <ul class="list-disc">
             <li><a href="q1/">Question 1</a></li>
-            <li><a href="q2/">Question 2</a></li>
-            <li><a href="q3/">Question 3</a></li>
         </ul>
     </main>
 </body>
