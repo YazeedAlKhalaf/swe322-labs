3ce695dfe2575eacc20eb976de04578979c20612
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-04-21 23:50:41 +0300

    remove the mysql commandf

--- watermelon/docker-entrypoint.sh
+++ watermelon/docker-entrypoint.sh
@@ -3,9 +3,6 @@
 # sleep 10 seconds, see why the thing below is getting connection failed
 sleep 10
 
-# Create the database if it doesn't exist
-mysql -h ${DB_HOST} -u root -p"${DB_PASS}" -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-
 # Run database migrations
 php vendor/bin/phinx migrate -c phinx.php
 
