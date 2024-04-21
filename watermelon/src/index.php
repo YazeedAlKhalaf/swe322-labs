e24d9d299ac71624bc190362b90be9a5b5f19421
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-04-21 18:15:48 +0300

    add some favicon stuff anf og image

--- watermelon/src/index.php
+++ watermelon/src/index.php
@@ -4,7 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Watermelon</title>
+
+    <!-- HTML Meta Tags -->
+    <title>Watermelon 🍉</title>
+    <meta name="description" content="Watermelon makes attendance taking a breeze! Your students scan a QR Code and attendance is taken. We use cutting edge technologies to make it possible :D">
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://watermelon.awesomedev.org">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Watermelon 🍉">
+    <meta property="og:description" content="Watermelon makes attendance taking a breeze! Your students scan a QR Code and attendance is taken. We use cutting edge technologies to make it possible :D">
+    <meta property="og:image" content="https://watermelon.awesomedev.org/static/images/opengraph.png">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="watermelon.awesomedev.org">
+    <meta property="twitter:url" content="https://watermelon.awesomedev.org">
+    <meta name="twitter:title" content="Watermelon 🍉">
+    <meta name="twitter:description" content="Watermelon makes attendance taking a breeze! Your students scan a QR Code and attendance is taken. We use cutting edge technologies to make it possible :D">
+    <meta name="twitter:image" content="https://watermelon.awesomedev.org/static/images/opengraph.png">
+
+    <!-- Meta Tags Generated via https://www.opengraph.xyz -->
+
+    <link rel="icon" type="image/svg+xml" href="/static/images/favicon.svg">
 </head>
 
 <body>
