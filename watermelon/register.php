15b21ed4c7a8f1c68c95846ce1f86325fc51cd20
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-04-28 00:40:48 +0300

    fix register page

--- watermelon/register.php
+++ watermelon/register.php
@@ -1,6 +1,24 @@
 <?php
-require_once 'lib/store/db.php';
+require_once './lib/store/db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $userStore->createUser("username", "password");
 }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+
+<body>
+    <h1>Register</h1>
+</body>
+
+</html>
